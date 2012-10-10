4b852fcfc7a37fdcf5553cc0838587bf647a55cd
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2012-10-10 11:04:04 -0300

    Updating Examples

--- examples/controllers/application_controller.rb
+++ examples/controllers/application_controller.rb
@@ -3,16 +3,23 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   # NotFound errors will be redirected to the action not_found while any other will be redirected to the action generic.
+  rescue_from ApiClient::Exceptions::Unauthorized, :with => :unauthorized_access
   rescue_from ApiClient::Exceptions::NotFound, :with => :not_found
   rescue_from ApiClient::Exceptions::Generic, :with => :generic
 
   # This code is only for example purposes.
   # Any behavior can be executed here. Just write your own.
-  def not_found
-    ApplicationMailer.send_not_found_notice.deliver
+  def unauthorized_access
+    redirect_to "http://www.example.com/sign_in?redirect_uri=#{request.url}"
   end
 
-  def generic
-    ApplicationMailer.send_generic_error.deliver
+  def not_found
+    ApplicationMailer.send_not_found_notice.deliver
+    render :file => "/public/404.html", :status => 404
+  end
+
+  def generic(exception)
+    ApplicationMailer.send_generic_error(exception).deliver
+    render :file => "/public/404.html", :status => 404
   end
 end
