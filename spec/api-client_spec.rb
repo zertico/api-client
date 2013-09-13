51f5b0df32ccd2ff6a0384077348633856b54213
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-09-13 17:00:12 -0300

    Added parallel specs

--- spec/api-client_spec.rb
+++ spec/api-client_spec.rb
@@ -1,8 +1,48 @@
 require 'spec_helper'
 
 describe ApiClient do
-  unless defined?(::Typhoues)
-    describe '.parallel' do
+  describe '.parallel' do
+    if defined?(::Typhoues)
+      before :each do
+        stub_request(:any, 'http://api.example.com/users/1').to_return(:body => {'a' => '1'}.to_json)
+        stub_request(:any, 'http://api.example.com/users/2').to_return(:body => {'a' => '2'}.to_json)
+        stub_request(:any, 'http://api.example.com/users/3').to_return(:body => {'a' => '3'}.to_json)
+        stub_request(:any, 'http://api.example.com/users/4').to_return(:body => {'a' => '4'}.to_json)
+        stub_request(:any, 'http://api.example.com/users/5').to_return(:body => {'a' => '5'}.to_json)
+        @user1 = User.new
+        @user2 = User.new
+        @user3 = User.new
+        @user4 = User.new
+        @user5 = User.new
+        ApiClient.parallel do
+          User.find(1).on_complete_update(@user1)
+          User.find(2).on_complete_update(@user2)
+          User.find(3).on_complete_update(@user3)
+          User.find(4).on_complete_update(@user4)
+          User.find(5).on_complete_update(@user5)
+        end
+      end
+
+      it 'initialize user1' do
+        @user1.attributes.should == {}
+      end
+
+      it 'initialize user2' do
+        @user2.attributes.should == {}
+      end
+
+      it 'initialize user3' do
+        @user3.attributes.should == {}
+      end
+
+      it 'initialize user4' do
+        @user4.attributes.should == {}
+      end
+
+      it 'initialize user5' do
+        @user5.attributes.should == {}
+      end
+    else
       it 'should raise an exception' do
         expect { ApiClient.parallel {} }.to raise_error(ApiClient::Exceptions::NotPossible)
       end
